4e30b03cfe1ce043941d8b88123d720e78e0a094
Muhathir . <muhathirmro@gmail.com>
2025-09-17 13:45:03 +0700

    Menyelesaikan Tutorial 3: Authentication dan Cookies

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 import uuid
 
 # Create your models here.
@@ -13,6 +14,7 @@ class News(models.Model):
         ('analysis', 'Analysis'),
     ]
     
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     content = models.TextField()
